7efff1b76cbd514763d719900a56e693657a5d5a
Alex Guha <alexandersguha@gmail.com>
2020-10-14 21:26:54 -0700

    Fleshed out Admin, and a bunch of other changes

--- flasktest.py
+++ flasktest.py
@@ -19,15 +19,15 @@ def send_css(path):
 	return send_from_directory('static', path)
 
 #Handles the form input from the stories page and puts it in feedback.txt
-@app.route('/stories/', methods=['POST', 'GET'])
-def stories():
+@app.route('/feedback/', methods=['POST', 'GET'])
+def feedback():
 	if request.method == 'POST':
 		first, last, email, feedback = request.form['firstname'], request.form['lastname'], request.form['email'], request.form['feedback']
 		with open('templates/feedback.txt', 'a') as file:
 			file.write(first + '\n' + last + '\n' + email + '\n' + feedback + '\r\n')
 		return render_template('form_success.html')
 	else:
-		return render_template('stories.html')
+		return render_template('feedback.html')
 
 @app.route('/about/')
 def about():
@@ -41,27 +41,43 @@ def blog():
 def notfound(e):
 	return render_template('404.html')
 
-@app.route('/admin/', methods=['POST', 'GET'])
-def admin():
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
 	if request.method == 'POST':
 		session.permanent = True
-		_user = request.form['user']
-		_password = request.form['pass']
-		session['user'] = _user
-		session['pass'] = _password
-		return redirect(url_for('user'))
-	else:
-		return render_template('admin.html')
-
-@app.route('/user/')
-def user():
-	if 'user' in session:
-		_user = session['user']
-		_pswrd = session['pass']
-		#auth check
-		return render_template('admin_actual.html')
-	else:
+		__user = request.form['user']
+		__password = request.form['pass']
+		session['user'] = __user
+		session['pass'] = __password
 		return redirect(url_for('admin'))
+	else:
+		return render_template('login.html')
+
+@app.route('/admin/')
+def admin():
+	if 'user' in session:
+		__user = session['user']
+		__pswrd = session['pass']
+		#auth check
+		__authorized = False
+		__permissions = 'user'
+		with open('admin.txt', 'r') as __auth_file:
+			for item in __auth_file.readlines():
+				__auth_user = eval(item)
+				if __auth_user[0] == __user and __auth_user[1] == __pswrd:
+					__authorized = True
+					__permissions = __auth_user[2]
+		if __authorized:
+			__admin_list = []
+			with open('admin.txt', 'r') as file:
+				__admin_list = map(eval, file.readlines())
+			return render_template('admin.html', admin_list=__admin_list, user=__user, permissions=__permissions)
+		else:
+			flash('You don\'t have permission to access the admin page.', 'info')
+			return redirect(url_for('login'))
+	else:
+		flash('You don\'t have permission to access the admin page.', 'info')
+		return redirect(url_for('login'))
 
 @app.route('/logout/')
 def logout():
@@ -69,7 +85,10 @@ def logout():
 		session.pop('user', None)
 		session.pop('pass', None)
 		flash('You have been logged out.', 'info')
-	return redirect(url_for('admin'))
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+# TODO
+# add encryption and access to files
